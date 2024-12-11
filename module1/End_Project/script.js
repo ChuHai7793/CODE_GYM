e3457e8a9e0eb7c603eb742d7cba8805b45aea2e
CHUHAI <haichu7793@gmail.com>
2024-12-11 21:34:48 +0700

    Jump, moveLeft, Right

--- module1/End_Project/script.js
+++ module1/End_Project/script.js
@@ -1,6 +1,6 @@
-import {animate} from "./animation.js";
+import { animateLoop} from "./animation.js";
 
-
+console.log(window.innerWidth);
 // const gangsterCanvas = document.getElementById('gangsterCanvas');
 // const ctx = gangsterCanvas.getContext('2d');
 // const CANVAS_WIDTH = gangsterCanvas.width = 128;
@@ -15,8 +15,8 @@ import {animate} from "./animation.js";
 // let gameFrame = 0;
 
 const gangsterImg = new Image();
-gangsterImg.src = 'Gangsters_1/Idle.png';
-animate(gangsterImg,5,8)
+// gangsterImg.src = 'resources/Idle.png';
+// animate(gangsterImg,5,8)
 
 // gangsterImg.src = 'Gangsters_1/Dead.png';
 // animate(gangsterImg,4,20)
@@ -24,6 +24,206 @@ animate(gangsterImg,5,8)
 // gangsterImg.src = 'Gangsters_1/Shot.png';
 // animate(gangsterImg,3,8)
 
+// gangsterImg.src = 'resources/Run.png';
+// animate(gangsterImg,5,8)
+
+
+const GANGSTER_WIDTH = 128;
+const GANGSTER_HEIGHT = 128;
+class Character {
+    constructor(characterId, x, y, state) {
+        this.x = x;
+        this.y = y;
+        this.character = document.getElementById(characterId);
+        this.state = state;
+        this.direction = "right";
+        this.characterImg = new Image();
+        this.FrameStats = {
+            maxFrames: 5,
+            staggerFrames: 8
+        };
+        this.jumpSpd = 10;
+        this.gravity = 10;
+        this.paddingX = 140;
+        this.animationId = undefined;
+        this.isRunning = false;
+
+        this.character.style.top = y + "px";
+        this.character.style.left = x + "px";
+    }
+
+    /* ------------------------------ HORIZONTAL MOVEMENT ------------------------------*/
+    // Function to move the character
+     run(units) {
+        if (!this.isRunning) return; // Stop if not running
+        this.x += units; // Update horizontal position
+
+         if (this.x > window.innerWidth - this.character.width - this.paddingX) {
+             this.x = this.x - units ;
+             this.character.style.left = this.x + "px"; // Apply new position
+         }else{
+             this.character.style.left = this.x + "px"; // Apply new position
+             this.animationId = requestAnimationFrame(()=>{this.run(units)}); // Continue animation
+         }
+
+    }
+
+    // Function to start running
+    moveRight(units) {
+        if (!this.isRunning) {
+            this.isRunning = true;
+            this.run(units); // Start animation loop
+        }
+    }
+
+    moveLeft(units) {
+        if (!this.isRunning) {
+            this.isRunning = true;
+            this.run(-units); // Start animation loop
+        }
+    }
+
+    // Function to stop running
+     stopRunning() {
+        this.isRunning = false;
+        cancelAnimationFrame(this.animationId); // Stop animation loop
+    }
+
+
+
+    /* ------------------------------ VERTICAL MOVEMENT ------------------------------*/
+
+    moveUp(units) {
+        this.y -= units;
+        this.character.style.top = this.y+ "px";
+    }
+
+    jump(event,maxHeight,originalHeight){
+        const interval = setInterval(() => {
+            if (this.y > maxHeight) {
+                this.moveUp(this.jumpSpd);
+            } else {
+                clearInterval(interval);
+                this.fall(originalHeight)
+            }
+        }, 20);
+    }
+
+    fall(originalHeight) {
+        const interval = setInterval(() => {
+            if (this.y < originalHeight) {
+                this.moveUp(-this.gravity);
+            } else {
+                clearInterval(interval);
+                this.state = 'Idle';
+                this.setAnimation();
+            }
+        },20)
+    }
+
+    changeDirection() {
+        if (this.direction === "left") {
+            this.character.style.transform =  "rotateY(180deg)";
+        } else {
+            this.character.style.transform =  "none";
+        }
+    }
+
+    setAnimation() {
+        if (this.state === 'start'){
+            this.characterImg.src = 'resources/Idle.png';
+            animateLoop(this.characterImg,this.FrameStats);
+        // } else if (this.state === 'jump'){
+        //     this.characterImg.src = 'resources/' + this.state + '.png';
+        //     drawFrame(this.characterImg,frameX)
+        } else {
+            this.characterImg.src = 'resources/' + this.state + '.png';
+        }
+        // console.log(this.FrameStats);
+        switch (this.state) {
+            case 'Idle':
+                this.FrameStats.maxFrames = 5;
+                this.FrameStats.staggerFrames = 8;
+                // cancelAnimationFrame(this.animationId);
+                // this.animationId = requestAnimationFrame(()=>{animateLoop(this.characterImg,5,8)});
+                // animateLoop(this.characterImg,5,8);
+                break;
+            case 'Run':
+
+                break;
+            case 'Dead':
+                this.FrameStats.maxFrames = 4;
+                this.FrameStats.staggerFrames = 20;
+                break;
+            case 'Shot':
+                this.FrameStats.maxFrames = 3;
+                this.FrameStats.staggerFrames = 8;
+                break;
+            case 'Jump':
+                PLAYER.FrameStats.maxFrames = 9;
+                PLAYER.FrameStats.staggerFrames = 20;
+
+                break;
+        }
+    }
+
+}
+
+
+
+const OFFSET_X = 0;
+const OFFSET_Y = 435;
+let PLAYER = new Character('gangsterCanvas',OFFSET_X,OFFSET_Y,'Idle');
+console.log(PLAYER.character.width )
+PLAYER.state = 'start';
+PLAYER.setAnimation();
+
+
+
+/*--------------------------- EVENTS --------------------------------*/
+let bodyElement = document.getElementsByTagName("body")[0];
+bodyElement.addEventListener("keydown", (event) => {
+    console.log(event.key);
+
+    switch (event.key) {
+        case 'a':
+            PLAYER.state = 'Run';
+            PLAYER.setAnimation();
+            PLAYER.moveLeft(5);
+            PLAYER.direction = 'left';
+            PLAYER.changeDirection();
+            break;
+        case 'd':
+            PLAYER.state = 'Run';
+            PLAYER.setAnimation();
+            PLAYER.moveRight(5);
+            PLAYER.direction = 'right';
+            PLAYER.changeDirection();
+            break;
+        case 'w':
+            if (event.key === 'w'){
+                if(PLAYER.state !== 'Jump'){
+                    PLAYER.state = 'Jump';
+                    PLAYER.setAnimation();
+                    PLAYER.jump(event,200,OFFSET_Y);
+                }
+            }
+
+    }
+});
+
+
+bodyElement.addEventListener("keyup", (event) => {
+        if (event.key === 'a'||event.key === 'd') {
+            PLAYER.stopRunning();
+
+        }
+        if (PLAYER.state !== 'Jump'){
+            // event.stopPropagation();
+            PLAYER.state = 'Idle';
+            PLAYER.setAnimation();
+        }
+})
 
 
 
@@ -32,86 +232,6 @@ animate(gangsterImg,5,8)
 
 
 
-
-
-
-
-
-
-// class Character {
-//     constructor(characterId,x,y,status) {
-//         this.x = x;
-//         this.y = y;
-//         this.character = document.getElementById(characterId);
-//         this.status = status;
-//         this.direction = "right"
-//
-//         this.character.style.top = y+"px";
-//         this.character.style.left = x+ "px";
-//     }
-//
-//     setAnimation(){
-//         this.character.src = "Gangsters_1/gif/"+this.status+".gif";
-//         console.log(this.character.src);
-//     }
-//
-//     moveRight(units) {
-//         this.x += units;
-//         this.character.style.left = this.x+ "px";
-//
-//     }
-//
-//     moveLeft(units) {
-//         this.x -= units;
-//         this.character.style.left = this.x+ "px";
-//
-//     }
-//
-//     changeDirection() {
-//         if (this.direction === "left") {
-//             this.character.style.transform =  "rotateY(180deg)";
-//         } else {
-//             this.character.style.transform =  "none";
-//         }
-//     }
-//
-// }
-// const SCREEN_HEIGHT = 600;
-// let player = new Character('player',0,SCREEN_HEIGHT,'Idle');
-//
-// player.status = 'Idle';
-// player.setAnimation();
-// console.log(player.character);
-// console.log(player.x);
-//
-// // player.moveRight(700);
-//
-// let bodyElement = document.getElementsByTagName("body")[0];
-// bodyElement.addEventListener("keypress", (event) => {
-//     console.log(event.key);
-//     if (event.key === 'a') {
-//         player.status = 'Run';
-//         player.setAnimation();
-//         player.direction = 'left';
-//         player.changeDirection();
-//
-//
-//         player.moveLeft(1);
-//     } else if (event.key === 'd') {
-//         player.status = 'Run';
-//         player.setAnimation();
-//         player.moveRight(1);
-//         player.direction = 'right';
-//         player.changeDirection();
-//     }
-// });
-// console.log(player.character);
-// console.log(bodyElement);
-
-
-
-//
-// player.style.left = "0px";
 
 
 
